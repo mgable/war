8671760ff14082f4472d68de5fad2df2a6eb1159
Mark Gable <mgable43@gmail.com>
2015-11-07 14:14:43 -0800

    added shuffle and deal methods

--- index.js
+++ index.js
@@ -3,31 +3,45 @@ var War = {};
 
 War.Deck = (function(){
 	var values = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"],
-    cards = create(values);
-  
-  function shuffle(cards){}
-  
-  function create(values){
-    return values.concat(values, values, values);
-  }
-  
-  function deal(players){}
-  
-  function compare(card_1, card_2){}
+		cards = shuffle(create(values));
 
-  return {
-    cards: cards
-  };
-  
+	function shuffle(cards){
+		var results = [];
+
+		while(cards.length){
+			var index = Math.round(Math.random() * (cards.length - 1) );
+			results.push(cards.splice(index,1)[0]);
+		}
+
+		return results;
+	}
+
+	function create(values){
+		return values.concat(values, values, values);
+	}
+
+	function deal(players){
+		var start = 0;
+		cards.forEach(function(v,i,a){
+			players[start].hand.push(v);
+			start = start >= (players.length - 1) ? 0 : start + 1;
+		});
+	}
+
+	function compare(cards){}
+
+	return {
+		cards: cards,
+		deal: deal
+	};
+
 })();
 
-console.info(War.Deck.cards);
-
 War.Player = (function(){
-	var constructor = function(hand){this.hand = hand;};
+	var constructor = function(){this.hand = [];};
 
 	 constructor.prototype = {
-	 	turn: function(){}
+		turn: function(){}
 	 };
 
 	 return constructor;
@@ -36,8 +50,10 @@ War.Player = (function(){
 
 War.Play = (function(){})();
 
-var player_1 = new War.Player([1,2,3]);
-var player_2 = new War.Player([4,5,6]);
+var player_1 = new War.Player();
+var player_2 = new War.Player();
+
+War.Deck.deal([player_1, player_2]);
 
 console.info(player_1.hand);
 console.info(player_2.hand);
